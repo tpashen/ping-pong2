9021c75286d08d67d0d64b1d41dd96b7c4e7c707
GeorToxa <38396607+GeorToxa@users.noreply.github.com>
2022-04-09 14:54:42 +0200

    Update game.py

--- game.py
+++ game.py
@@ -3,7 +3,29 @@
 #2
 
 #3
+# Game Scene
+from turtle import window_height
 
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = True
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player("racket.png", 30, 200, 4, 50, 150)
+racket2 = Player("racket.png", 520, 200, 4, 50, 150)
+ball = GameSprite("tenis_ball.png", 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font("calibri.ttf", 35)
+lose1 = font.render("PLAYER 1 LOSE!", True, (180, 0, 0))
+lose2 = font.render("PLAYER 2 LOSE!", True, (180, 0, 0))
 #4
 
 #5
